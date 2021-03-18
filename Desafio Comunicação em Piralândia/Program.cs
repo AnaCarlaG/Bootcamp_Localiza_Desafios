a6d83304aa4d3c1a4cbacd445d60be28db5b7bbb
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2021-03-18 19:50:22 -0300

    Desafio para inverter uma string

---
+++ Desafio Comunicação em Piralândia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+public class OrdenacaoInvertida {
+
+	public static void Main(string[] args) { 
+
+            string numeros = Console.ReadLine();
+
+            Console.WriteLine("{0}\n",InverteOrdenacaoString(numeros));
+    }
+    	public static string InverteOrdenacaoString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+		}
+}
