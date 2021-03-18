f70a948c7016118ae369e780010a945e627c3184
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2021-03-18 18:32:13 -0300

    Desafio do consumo médio

---
+++ Desafio do Consumo Médio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ConsumoMedio
+{
+
+    public static void Main(string[] args)
+    {
+
+        var distanciaPercorrida = Convert.ToInt32(Console.ReadLine());
+        var combustivelGasto = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("{0:0.000} km/l", distanciaPercorrida / combustivelGasto);
+    }
+}
