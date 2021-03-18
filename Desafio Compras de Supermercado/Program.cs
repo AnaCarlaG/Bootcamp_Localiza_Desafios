988f075e38b86d717e183c51b0d787e73ac9d0d8
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2021-03-18 20:26:10 -0300

    desafio da lista de compras

---
+++ Desafio Compras de Supermercado/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MinhaClasse
+{
+    public static void Main(string[] args)
+    {
+        var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+        String[] listaCompras;
+        SortedSet<string> lista;
+
+        for (int i = 0; i < totalDeCasosDeTeste; i++)
+        {
+            listaCompras = Console.ReadLine().Split(' ');
+            lista = new SortedSet<string>(listaCompras);
+
+            foreach (var n in lista)
+            {
+                Console.Write(n.Replace(" ",""));
+                Console.Write(' ');
+            }
+            lista.Clear();
+            Console.WriteLine();
+        }
+    }
+}
