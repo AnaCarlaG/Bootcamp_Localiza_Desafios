00029c5df862fda165ba9f49ff3ecdd45bcee5a5
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2021-03-18 20:07:10 -0300

    desafio da coordenada dos quadrantes

---
+++ Desafio Coordenadas de um ponto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System; 
+
+public class URI {
+
+	public static void Main(string[] args) { 
+
+          float x, y;
+          
+          string[] arrValor = Console.ReadLine().Replace(",",".").Split(' ');
+          x = float.Parse(arrValor[0]);
+          y = float.Parse(arrValor[1]);
+		
+		if(x == 0.0 && y == 0.0){
+		Console.WriteLine("Origem");
+		}
+		else if(y == 0){
+		  Console.WriteLine("Eixo X");
+		}
+		else if(x == 0){
+		  Console.WriteLine("Eixo Y");
+		}
+		else if (x > 0 && y > 0){
+			Console.WriteLine("Q1");
+		}
+		else if (x < 0 && y > 0){
+			Console.WriteLine("Q2");
+		}
+		else if (x < 0 && y < 0){
+			Console.WriteLine("Q3");
+		}
+		else if (x > 0 && y < 0){
+			Console.WriteLine("Q4");
+		}
+			
+
+    }
+
+}
